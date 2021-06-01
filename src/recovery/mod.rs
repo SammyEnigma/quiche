548c9ffb1fef1e9607b0a8e3b1133fad081c0fb4
Junho Choi <junho@cloudflare.com>
2021-06-01 14:33:06 -0700

    recovery: RFC6937 Proportional Rate Reduction
    Implmenting RFC6937 (Proportional Rate Reduction for TCP).
    
    Currently when we go into the recovery, retransmiting
    lost packet will wait until bytes_in_flight < cwnd. This
    is described as "half window of silence" in RFC6937 because
    it took a half rtt to get ACK to open a space for transmission.
    
    PRR tries to send such retransmissions (not limited to new or
    retransmitted packet though) more evenly at the beginning of
    the recovery, does 'soft pacing' for retransmissions for stable
    and fast recovery.
    
    Here is some differences from TCP algorithm described in the RFC.
    
    - pipe: RFC6675 pipe definition is more close to QUIC's
      bytes-in-flight since it includes retransmit already.
      So here bytes_in_flight is used instead of pipe.
    - RecoverFS: use bytes_in_flight value in the beginning of
      the recovery.
    - Between PRR-CRB and PRR-SSRB, PRR-SSRB is implemented
      when pipe <= ssthresh.
    
    Note that PRR is only enabled in CUBIC.

--- src/recovery/mod.rs
+++ src/recovery/mod.rs
@@ -133,6 +133,9 @@ pub struct Recovery {
     pacing_rate: u64,
 
     last_packet_scheduled_time: Option<Instant>,
+
+    // RFC6937 PRR.
+    prr: prr::PRR,
 }
 
 impl Recovery {
@@ -208,6 +211,8 @@ impl Recovery {
             pacing_rate: 0,
 
             last_packet_scheduled_time: None,
+
+            prr: prr::PRR::default(),
         }
     }
 
@@ -238,6 +243,8 @@ impl Recovery {
                 (self.bytes_in_flight + sent_bytes) < self.congestion_window;
 
             self.on_packet_sent_cc(sent_bytes, now);
+
+            self.prr.on_packet_sent(sent_bytes);
 
             self.set_loss_detection_timer(handshake_status, now);
         }
@@ -521,7 +528,9 @@ impl Recovery {
             return std::usize::MAX;
         }
 
-        self.congestion_window.saturating_sub(self.bytes_in_flight)
+        // Open more space (snd_cnt) for PRR when allowed.
+        self.congestion_window.saturating_sub(self.bytes_in_flight) +
+            self.prr.snd_cnt
     }
 
     pub fn rtt(&self) -> Duration {
@@ -1764,4 +1773,5 @@ mod tests {
 mod cubic;
 mod delivery_rate;
 mod hystart;
+mod prr;
 mod reno;
